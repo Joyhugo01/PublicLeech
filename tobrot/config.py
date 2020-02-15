071989f2bb4dd2f6b70152e091d2ab586613d401
Joyhugo01 <54698383+Joyhugo01@users.noreply.github.com>
2020-02-15 19:09:00 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -1,30 +1,7 @@
-import os
-from tobrot.config
+from tobrot.sample_config import Config
+
 class Config(Config):
-    # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1093731084:AAHY0bIgN9vX3YGExOOOqir650Tm2DzLtSc")
-    # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 801630))
-    API_HASH = os.environ.get("09441fe19570a1557cc3f0ee5f2ea843")
-    # Get these values from my.telegram.org
-    # to store the channel ID who are authorized to use the bot
-    AUTH_CHANNEL = int(os.environ.get("AUTH_CHANNEL", -373543057))
-    # the download location, where the HTTP Server runs
-    DOWNLOAD_LOCATION = "./DOWNLOADS"
-    # Telegram maximum file upload size
-    MAX_FILE_SIZE = 50000000
-    TG_MAX_FILE_SIZE = 1572864000
-    FREE_USER_MAX_FILE_SIZE = 50000000
-    # chunk size that should be used with requests
-    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
-    # default thumbnail to be used in the videos
-    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
-    # maximum message length in Telegram
-    MAX_MESSAGE_LENGTH = 4096
-    # set timeout for subprocess
-    PROCESS_MAX_TIMEOUT = 3600
-    #
-    ARIA_TWO_STARTED_PORT = int(os.environ.get("ARIA_TWO_STARTED_PORT", 6800))
-    EDIT_SLEEP_TIME_OUT = int(os.environ.get("EDIT_SLEEP_TIME_OUT", 1))
-    MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START = int(os.environ.get("MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START", 600))
-    MAX_TG_SPLIT_FILE_SIZE = int(os.environ.get("MAX_TG_SPLIT_FILE_SIZE", 1072864000))
+  TG_BOT_TOKEN = "1093731084:AAHY0bIgN9vX3YGExOOOqir650Tm2DzLtSc"
+  APP_ID = 801630
+  API_HASH = "09441fe19570a1557cc3f0ee5f2ea843"
+  AUTH_CHANNEL = -373543057	
