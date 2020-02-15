0290216c03da8600af1a3788f5450630bcb89fb6
Joyhugo01 <54698383+Joyhugo01@users.noreply.github.com>
2020-02-15 18:48:48 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -1,5 +1,5 @@
 import os
-
+from tobrot.sample_config import Config
 class Config(object):
     # get a token from @BotFather
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1093731084:AAHY0bIgN9vX3YGExOOOqir650Tm2DzLtSc")
